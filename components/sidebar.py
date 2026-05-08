143889ed6bf49db13e422a5f82ab557b62b1e649
Wooz27 <reymanz7000@gmail.com>
2026-05-07 21:10:34 -0600

    Estefani

--- components/sidebar.py
+++ components/sidebar.py
@@ -3,7 +3,7 @@ import streamlit as st
 def sidebar():
     st.sidebar.title("Hola, quien esta trabajando? 👋")
     with st.sidebar.expander("Personal"):
-        personal = st.radio("Selecciona tu usuario", options=["Alex", "Silvia", "Erik", "Lourdes", "Otro"], key = "personal_selection")
+        personal = st.radio("Selecciona tu usuario", options=["Estefani", "Silvia", "Erik", "Lourdes", "Otro"], key = "personal_selection")
         st.markdown("## Recuerda que tienes que seleccionar tu usuario para que las reservas se asignen correctamente")
 
         
